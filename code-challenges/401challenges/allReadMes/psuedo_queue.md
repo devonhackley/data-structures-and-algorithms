80f4234898df3487ffe54173c28c51fe659190b1
Devon Hackley <devonhackley@gmail.com>
2019-06-11 11:58:55 -0700

    finished with coding challenge

---
+++ code-challenges/401challenges/allReadMes/psuedo_queue.md
@@ -0,0 +1,27 @@
+# Stack and Queues
+This read me contains information about my psuedo-queue implementation.
+
+## Challenge
+Implement a queue using two Stacks.
+
+## Approach & Efficiency
+ * Dequeue: 
+     * Time: O(n) Space: O(1)
+ * Enqueue: 
+     * Time: O(1) Space: O(1) 
+
+## API
+  ### Methods:
+Queue:
+* dequeue(): does not take any argument, removes the node from the front of the queue, and returns the node’s value.
+* enqueue(int num): which takes any value as an argument and adds a new node with that value to the back of the queue
+
+
+[Link to Psuedo-Queue Code](../src/main/java/challenges/QueueWithStacks/PsuedoQueue.java)
+[Link to Psuedo-Queue Test](../src/test/java/challenges/QueueWithStacks/PsuedoQueueTest.java)
+
+## Whiteboard
+![pic](../assets/whiteboardC11.jpg)
+
+
+
---
+++ code-challenges/401challenges/src/main/java/challenges/QueueWithStacks/PsuedoQueue.java
@@ -0,0 +1,54 @@
+package challenges.QueueWithStacks;
+
+import challenges.stacksandqueues.Stack;
+
+public class PsuedoQueue {
+    protected Stack stack1;
+    protected Stack stack2;
+
+    public PsuedoQueue() {
+        this.stack1 = new Stack();
+        this.stack2 = new Stack();
+    }
+
+    public Stack getStack1() {
+        return stack1;
+    }
+
+    public void setStack1(Stack stack1) {
+        this.stack1 = stack1;
+    }
+
+    public Stack getStack2() {
+        return stack2;
+    }
+
+    public void setStack2(Stack stack2) {
+        this.stack2 = stack2;
+    }
+
+
+
+    public void enqueue(int value){
+        this.stack1.push(value);
+    }
+
+    public int dequeue(){
+       try{
+           while(this.stack1.getTop() != null){
+               this.stack2.push(this.stack1.pop());
+           }
+           // take value from the "back"
+           int top = this.stack2.pop();
+
+           while(this.stack2.getTop() != null ){
+               // grab all values from stack 2 and add to "back" of stack1(queue)
+               this.stack1.push(this.stack2.pop());
+           }
+           return top;
+       } catch (NullPointerException ex){
+            System.out.println("The stack is empty");
+            return -1;
+       }
+    }
+}
